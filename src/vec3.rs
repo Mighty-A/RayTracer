d7d4ec74ad239dded1ad4fe5df4b2709c39cd834
yhy <792140335@qq.com>
2020-07-20 11:24:51 +0800

    7.20 fix code format

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, AddAssign, Sub, SubAssign, Mul, MulAssign, Div, Neg};
+use std::ops::{Add, AddAssign, Div, Mul, MulAssign, Neg, Sub, SubAssign};
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Vec3 {
@@ -158,7 +158,7 @@ impl Div<f64> for Vec3 {
     fn div(self, other: f64) -> Self {
         Self {
             x: self.x / other,
-            y: self.y / other, 
+            y: self.y / other,
             z: self.z / other,
         }
     }
@@ -173,7 +173,7 @@ impl Vec3 {
         }
     }
 
-    fn cross (a: Self, b: Self) -> Self {
+    fn cross(a: Self, b: Self) -> Self {
         Self {
             x: a.y * b.z - a.z * b.y,
             y: b.x * a.z - a.x * b.z,
@@ -246,14 +246,13 @@ mod tests {
         )
     }
 
-    
     #[test]
     fn test_add_assign_f64() {
         let mut x = Vec3::new(1.0, 0.0, -1.0);
         x += 233.0;
         assert_eq!(x, Vec3::new(234.0, 233.0, 232.0))
     }
-    
+
     #[test]
     fn test_sub() {
         assert_eq!(
@@ -261,7 +260,7 @@ mod tests {
             Vec3::new(-1.0, -4.0, -7.0)
         )
     }
-    
+
     #[test]
     fn test_sub_assign() {
         let mut x = Vec3::new(1.0, 0.0, -1.0);
@@ -280,7 +279,7 @@ mod tests {
         x -= 1.0;
         assert_eq!(x, Vec3::new(0.0, -1.0, -2.0))
     }
-    
+
     #[test]
     fn test_mul() {
         assert_eq!(
@@ -307,7 +306,7 @@ mod tests {
         x *= 1.0;
         assert_eq!(x, Vec3::new(1.0, 0.0, -1.0));
     }
-    
+
     #[test]
     fn test_div() {
         assert_eq!(Vec3::new(1.0, -2.0, 0.0) / 2.0, Vec3::new(0.5, -1.0, 0.0));
@@ -320,7 +319,7 @@ mod tests {
             Vec3::new(1.0, 4.0, 9.0)
         );
     }
-    
+
     #[test]
     fn test_cross() {
         assert_eq!(
@@ -333,13 +332,12 @@ mod tests {
     fn test_neg() {
         assert_eq!(-Vec3::new(1.0, -2.0, 3.0), Vec3::new(-1.0, 2.0, -3.0));
     }
-    
+
     #[test]
     fn test_squared_length() {
         assert_eq!(Vec3::new(1.0, 2.0, 3.0).squared_length(), 14.0 as f64);
     }
 
-    
     #[test]
     fn test_length() {
         assert_eq!(
@@ -350,10 +348,7 @@ mod tests {
 
     #[test]
     fn test_unit() {
-        assert_eq!(
-            Vec3::new(233.0, 0.0, 0.0).unit(),
-            Vec3::new(1.0, 0.0, 0.0)
-        );
+        assert_eq!(Vec3::new(233.0, 0.0, 0.0).unit(), Vec3::new(1.0, 0.0, 0.0));
         assert_eq!(
             Vec3::new(-233.0, 0.0, 0.0).unit(),
             Vec3::new(-1.0, 0.0, 0.0)
@@ -365,5 +360,4 @@ mod tests {
     fn test_unit_panic() {
         Vec3::new(0.0, 0.0, 0.0).unit();
     }
-    
 }
